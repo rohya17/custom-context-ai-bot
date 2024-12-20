68a40d52880db22e8564f3df2aa6ab4493efe30b
ROHIT\DELL <thoraverohit17895@gmail.com>
2024-12-20 16:05:49 +0530

    Project initialization

---
+++ src/main/java/com/ai/context/custom/bot/CustomContextAiBotApplication.java
@@ -0,0 +1,13 @@
+package com.ai.context.custom.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomContextAiBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomContextAiBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/context/custom/bot/CustomContextAiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.context.custom.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomContextAiBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
